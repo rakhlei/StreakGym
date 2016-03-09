30acaa03229bfda8724a64c3b180d5a8d9a62779
Emily Hughes <hughese@miamioh.edu>
2016-03-09 18:06:37 -0500

    New file stucture, use these

--- js/app.js
+++ js/app.js
@@ -2,17 +2,17 @@ var example = angular.module("example", ['ui.router']);
 
 example.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
-        .state('index', {
-	    url: '/index',
-	    templateUrl: 'index.html',
+        .state('login', {
+	    url: '/login',
+	    templateUrl: 'web/login.html',
 	    controller: 'LoginController'
 	})
         .state('index', {
 	    url: '/index',
-	    templateUrl: 'index.html',
+	    templateUrl: 'web/index.html',
 	    controller: 'SecureController'
 	});
-    $urlRouterProvider.otherwise('/index');
+    $urlRouterProvider.otherwise('/login');
 });
 
 example.controller("LoginController", function($scope) {
@@ -20,8 +20,8 @@ example.controller("LoginController", function($scope) {
     $scope.login = function() {
 
         // Change the following to your application's parameters
-        var client_id = "227GZJ";
-        var redirect_uri = "http%3A%2F%2Fceclnx01.cec.miamioh.edu%2Fusers%2Fhughese%2Fcec205%2F%2Fstreak%2Foauth_callback.html";
+        var client_id = "227NPW";
+        var redirect_uri = "http%3A%2F%2Fceclnx01.cec.miamioh.edu%2Fusers%2Fhughese%2Fcec205%2Fsite%2Foauth_callback.html";
         var scope = "activity%20profile";
 
 	window.location.href = "https://www.fitbit.com/oauth2/authorize?client_id=" + client_id + "&response_type=token" + "&scope=" + scope + "&redirect_uri=" + redirect_uri;
@@ -35,7 +35,7 @@ example.controller("SecureController", function($scope) {
     $scope.accessToken = JSON.parse(window.localStorage.getItem("fitbit")).oauth.access_token;
     $scope.userId = JSON.parse(window.localStorage.getItem("fitbit")).oauth.account_userid;
 
-    // Make calls with the accesstoken either here or with inline js in ../secure.html
+    // Make calls with the accesstoken either here or with inline js in ../templates/secure.html
 
     var xhttp;
     if (window.XMLHttpRequest){
@@ -50,11 +50,11 @@ example.controller("SecureController", function($scope) {
 
     xhttp.onreadystatechange = function() {
        if (xhttp.readyState == 4 && xhttp.status == 200) {
-          $scope.profile = JSON.parse(xhttp.responseText).user.fullName;
+          $scope.activity = JSON.parse(xhttp.responseText).activities.steps;
 
           // Use angular to get the div to change
-          var myElement = angular.element( document.querySelector('#username'));
-          myElement.text($scope.profile);
+          var myElement = angular.element( document.querySelector('#selector'));
+          myElement.text($scope.activity);
        }
     };
 
